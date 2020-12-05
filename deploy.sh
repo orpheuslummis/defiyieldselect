4812051cd24d9e6b7eb32c0ffbaabe909cfe31eb
Orpheus Lummis <o@orpheuslummis.info>
2020-12-05 11:25:23 -0600

    cleaner deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rsync -av --exclude .venv \
     --exclude __pycache__ \
     --exclude data \
     . equivos@45.56.67.145:apps/$PROJECT
-ssh -t equivos@45.56.67.145 "cd /home/equivos/apps/$PROJECT && docker-compose up --build -d"
+ssh -t equivos@45.56.67.145 "cd \$HOME/apps/$PROJECT && docker-compose up --build -d"
