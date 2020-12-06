e66f738f906d1b1374c656ea26c6a4541d454b35
Orpheus Lummis <o@orpheuslummis.info>
2020-12-06 17:01:03 -0600

    deploy script more ignores

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ rsync -av --exclude .venv \
     --exclude .git \
     --exclude __pycache__ \
     --exclude data \
+    --exclude tmp \
+    --exclude results \
     . equivos@45.56.67.145:apps/$PROJECT
 ssh -t equivos@45.56.67.145 "cd \$HOME/apps/$PROJECT && docker-compose up --build -d"
