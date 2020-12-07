60d23c3c623a73026783cb74ccfd8f52569217bd
Orpheus Lummis <o@orpheuslummis.info>
2020-12-07 07:34:40 -0600

    rsyncing the minimum (slow internet)

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ rsync -av --exclude .venv \
     --exclude data \
     --exclude tmp \
     --exclude results \
+    --exclude go \
+    --exclude jupyter \
     . equivos@45.56.67.145:apps/$PROJECT
 ssh -t equivos@45.56.67.145 "cd \$HOME/apps/$PROJECT && docker-compose up --build -d"
