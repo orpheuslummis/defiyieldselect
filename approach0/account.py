ff52c8026c61789429d694edaa58e028c43cd91e
Orpheus Lummis <o@orpheuslummis.info>
2020-12-08 08:19:51 -0600

    approach0 can go now

--- approach0/account.py
+++
@@ -1,8 +0,0 @@
-class Account:
-    pass
-
-    # private key
-
-    # considering the protocols we support, establish where we are
-    
-    # did we enter compound? 
--- approach0/query.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_historical_rates(protocol, asset):
-    return get_yieldfi_rate(protocol, asset)
-
-# NOTE this service might not stay available, and it doesn't give historical data since the beginning
-def get_yieldfi_rate(protocol, asset):
-    return requests.get(f"https://api.yield.fi/lending/rate?protocol={protocol}&asset={asset}").json()
