9a08066da895c9c16f8261ff232f9a4949079168
Orpheus Lummis <o@orpheuslummis.info>
2020-12-22 16:55:14 -0600

    no need for that docstring

--- src/value.py
+++ src/value.py
@@ -5,12 +5,6 @@ from config import *
 
 
 def value(data: pd.DataFrame) -> pd.Series:
-    '''
-    we combine forecasted features (price, apr) into a value multiplier
-    the base is 1, therefore a value of 1.37 means 37% increase
-
-    output: timeseries of (value + compounded_interested(t))
-    '''
     apr_multiplier = (data['apr'] * PERIOD_IN_YEARS[TIMESTEP_PERIOD]) + 1
     value_pred = apr_multiplier.cumprod() * data['price']
     return value_pred
