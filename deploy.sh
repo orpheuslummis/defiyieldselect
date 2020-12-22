4cb246f962db9ffbeb96c8b14b05888f17ef2cce
Orpheus Lummis <o@orpheuslummis.info>
2020-12-22 13:41:18 -0600

    include some base data

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,6 @@ PROJECT=defiyieldoptimization
 rsync -av --exclude .venv \
     --exclude .git \
     --exclude __pycache__ \
-    --exclude tmp \
     --exclude results \
-    --exclude results_dl \
-    --exclude go \
-    --exclude jupyter \
-    --exclude data \
-    --include data/bowhead_csvs \
     . equivos@45.56.67.145:apps/$PROJECT
 ssh -t equivos@45.56.67.145 "cd \$HOME/apps/$PROJECT && docker-compose up --build -d"
