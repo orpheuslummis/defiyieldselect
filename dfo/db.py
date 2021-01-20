deb583ad2149a46c2cec4d3014b28d4874db380a
Orpheus Lummis <o@orpheuslummis.info>
2021-01-20 15:17:58 -0600

    keep db fresh

--- dfo/db.py
+++ dfo/db.py
@@ -1,10 +1,10 @@
-# in case the app runs forever, we only keep the latest hour of information to avoid overflow
-# using utc datetimes
+import time
 
+import datetime
 from peewee import (CharField, Database, DateTimeField, FloatField, Model,
                     SqliteDatabase, fn)
 
-from dfo.config import DATA_PATH
+from dfo.config import DATA_PATH, PAST_HORIZON
 
 database = SqliteDatabase(f'{DATA_PATH}/dfo.db', pragmas={'journal_mode': 'wal'}) 
 
@@ -58,3 +58,19 @@ def latest_scores() -> dict:
         for result in q_results:
             results[result['token']] = result['score']
         return results
+
+
+def fresh_only() -> None:
+    """
+    periodically removing old entries
+    in case the app runs forever, to avoid overflow
+    """
+    while True:
+        stale = datetime.datetime.utcnow() - datetime.timedelta(seconds=PAST_HORIZON * 2)
+        database = prepared_db()
+        with database:
+            n_apr = APR.delete().where(APR.datetime < stale).execute()
+            n_price = Price.delete().where(Price.datetime < stale).execute()
+            n_result = Result.delete().where(Result.datetime < stale).execute()
+            print(f'cleared database entries older than {stale}. APR {n_apr}, Price {n_price}, Result {n_result} ')
+        time.sleep(PAST_HORIZON)
--- run.py
+++ run.py
@@ -3,8 +3,10 @@ import threading
 import dfo.collect
 import dfo.score
 import dfo.serve
+import dfo.db
 
 if __name__ == "__main__":
     threading.Thread(target=dfo.collect.run).start()
     threading.Thread(target=dfo.score.run).start()
     threading.Thread(target=dfo.serve.run).start()
+    threading.Thread(target=dfo.db.fresh_only).start()
