2eee250ed66d02e760ff0d7d6da8ec50b1e794d1
Orpheus Lummis <o@orpheuslummis.info>
2021-01-23 14:37:28 -0600

    scoring reorg

--- dfo/score.py
+++ dfo/score.py
@@ -20,38 +20,78 @@ from dfo.db import APR, Price, Result, prepared_db
 database = prepared_db()
 
 
-def get_recent_dataframe_token(modelname: str, token_apr: str) -> Optional[pd.DataFrame]:
+def model_a(token_apr: str, data_price: pd.DataFrame, data_apr: pd.DataFrame) -> float:
     """
-    return recent data of selected model
-    normalizing sampling rate to SAMPLING_INTERVAL
-    recentness is parameterized by PAST_HORIZON
-    a dataframe might have a different number of measurements than its complementary
+    approach: multiply APR by a weighted slope of linear regression of price
     """
+    reg_price = linregress(data_price.seconds_since, data_price.value)
+    price_factor = (1 + reg_price.slope * PRICE_WEIGHT)
+    apr_mean = data_apr['value'].mean()
+    score = price_factor * apr_mean
+    if DEBUG: print(f"model_a: {token_apr} {price_factor=} * {apr_mean=} = {score=}")
+    # price_stddev = data_price.std().value
+    # apr_stddev = data_apr.std().value
+    # print(f'{price_stddev=}')
+    # print(f'{apr_stddev=}')
+    return score
+
+
+def model_b(token_apr: str, data_price: pd.DataFrame, data_apr: pd.DataFrame) -> float:
+    """
+    approach: multiply APR by the cubed slope of linear regression of price
+    """
+    reg_price = linregress(data_price.seconds_since, data_price.value)
+    price_factor = (1 + reg_price.slope)**3
+    apr_mean = data_apr['value'].mean()
+    score = price_factor * apr_mean
+    if DEBUG: print(f"model_b: {token_apr} {price_factor=} * {apr_mean=} = {score=}")
+    return score
+
+
+def get_recent_dataframe_token(modelname: str, token_apr: str, now: datetime) -> Optional[pd.DataFrame]:
+    """
+    Return recent data of selected model
+    
+    Recentness is parameterized by PAST_HORIZON
+    Normalizing sampling rate to SAMPLING_INTERVAL
+    We assume the APR values are normalized in an APR (in practice each platform has a diffferent APR definition and we don't normalize that here). We divide by their mantissa to get a percentage rate.
+    We normalize the price values by diviving them by their mean
+
+    Complementary dataframe (APR for Price, Price for APR) won't necessarily have the same number of measurements
+    """
+    # obtain data
+    def get_data(model, token: str) -> pd.DataFrame:
+        # there probably is a more efficient way than -> dict -> list -> dataframe
+        data = pd.DataFrame(list(model.select().where(
+            model.token == token,
+            model.datetime >= now - timedelta(seconds=PAST_HORIZON),
+        ).dicts())) 
+        data = data.drop(columns=['id'])
+        return data
     if modelname == 'APR':
-        model = APR
         token = token_apr
+        data = get_data(APR, token)
+        # to percentage
+        data.value = data.value.div(ORCA_API_MANTISSA)
     elif modelname == 'price':
-        model = Price
         token = APR_TOKEN_TO_UNISWAPV2_TOKENS[token_apr]
+        data = get_data(Price, token)
+        # normalize for comparable slopes
+        data.value = data.value/data.value.mean()
     else:
         raise Exception
-    # obtain from db
-    past_time_lower_bound = datetime.now(timezone.utc) - timedelta(seconds=PAST_HORIZON)
-    data = pd.DataFrame(list(model.select().where(
-        model.datetime >= past_time_lower_bound,
-        model.token == token
-    ).dicts())) # there probably is a more efficient way than -> dict -> list -> dataframe
     if len(data) == 0:
         return None
-    data = data.drop(columns=['id'])
-    # resampling
+    # resample
     data.datetime = pd.to_datetime(data.datetime)
     data = data.set_index('datetime')
     data  = data.resample(SAMPLING_INTERVAL).mean().bfill()
-    # obtaining time relative to beginning of frame for regression
+    # obtain time relative to beginning of frame for regression
     data = data.reset_index()
     data.datetime = pd.to_numeric(data.datetime)
     data['seconds_since'] = (data.datetime - data.datetime[0])/int(1e9)
+    # print(f'{modelname=}, {token=}')
+    # print(f'{data}')
     return data
 
 
@@ -61,32 +101,6 @@ def dataframe_common_length(dfa: pd.DataFrame, dfb: pd.DataFrame) -> (pd.DataFra
     dfb = dfb[-min_length:]
     assert dfa.shape == dfb.shape
     return (dfa, dfb)
-
-
-def model_a(token_apr: str, data_price: pd.DataFrame, data_apr: pd.DataFrame) -> float:
-    """
-    approach: multiply APR by a weighted slope of linear regression of price
-    """
-    data_price, data_apr = dataframe_common_length(data_price, data_apr)
-    reg_price = linregress(data_price.seconds_since, data_price.value)
-    price_factor = (1 + reg_price.slope * PRICE_WEIGHT)
-    apr_mean = data_apr['value'].div(ORCA_API_MANTISSA).mean()
-    score = price_factor * apr_mean
-    if DEBUG: print(f"model_a: {token_apr} {price_factor=} * {apr_mean=} = {score=}")
-    return score
-
-
-def model_b(token_apr: str, data_price: pd.DataFrame, data_apr: pd.DataFrame) -> float:
-    """
-    approach: multiply APR by the cubed slope of linear regression of price
-    """
-    data_price, data_apr = dataframe_common_length(data_price, data_apr)
-    reg_price = linregress(data_price.seconds_since, data_price.value)
-    price_factor = (1 + reg_price.slope)**3
-    apr_mean = data_apr['value'].div(ORCA_API_MANTISSA).mean()
-    score = price_factor * apr_mean
-    if DEBUG: print(f"model_b: {token_apr} {price_factor=} * {apr_mean=} = {score=}")
-    return score
 
 
 def store_result(datetime: datetime, token: str, modelid: str, score: float) -> None:
@@ -100,17 +114,20 @@ def store_result(datetime: datetime, token: str, modelid: str, score: float) -> 
 def run() -> None:
     models = [
         model_a,
-        model_b
+        model_b,
     ]
     while True:
         time.sleep(INTERVAL)
         now = datetime.now(timezone.utc)
         for token_apr in APR_TOKENS:
-            data_apr = get_recent_dataframe_token('APR', token_apr)
-            data_price = get_recent_dataframe_token('price', token_apr)
-            if data_apr is None or data_price is None or len(data_apr) < 2 or len(data_price) < 2:
-                print(f'scoring: skipping {token_apr} because we don\'t have enough recent data')
+            data_apr = get_recent_dataframe_token('APR', token_apr, now)
+            data_price = get_recent_dataframe_token('price', token_apr, now)
+            if data_apr is None or data_price is None or len(data_apr) > 2 or len(data_price) >2:
+                if DEBUG: print(f'scoring: skipping {token_apr} because we don\'t have enough recent data')
             else:
-                for model in models:
-                    token_score = model(token_apr, data_price, data_apr)
-                    store_result(datetime=now, token=token_apr, modelid=model.__name__, score=token_score)
+                data_price, data_apr = dataframe_common_length(data_price, data_apr)
+                print(f'{data_apr=}')
+                print(f'{data_price=}')
+                # for model in models:
+                #     token_score = model(token_apr, data_price, data_apr)
+                #     store_result(datetime=now, token=token_apr, modelid=model.__name__, score=token_score)
